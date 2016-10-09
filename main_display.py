1f07d1d713dcdc31a08bcdadae2e24b6c9a9671f
ShivonQ <realitykeeper@gmail.com>
2016-10-09 17:14:52 -0500

    Everything is broken because of me breaking it up into classes, in the middle of it. Creating a text manager, and a graphics manager and a map manager

--- main_display.py
+++ main_display.py
@@ -10,9 +10,15 @@ class Main_Display:
         self.window_height = 600
 
         pygame.init()
-        MAIN_SURFACE = pygame.display.set_mode((self.window_width,self.window_height))
+        MAIN_SURFACE = pygame.display.set_mode((self.window_width, self.window_height))
         pygame.display.set_caption('Simple RPG')
 
+
+    def render_start_menu(self):
+        print('This is working hopefully')
+    #     Hopefully I will be able to get the listing of Heroes here
+    #     Or I will set it up so that a user can only have 1 hero at a time,
+    #     but can ditch their hero and start over
 
 
     def render_inventory_menu(self,base_surf,hero):
@@ -21,12 +27,38 @@ class Main_Display:
 #         TODO: Create Subsurface that interfaces with Inventory of game
 
     # This will render a xp bar that is filled according to the Heros xp
+
+    def render_char_stat_display(self,base_surf,hero):
+        x_y_wid_hei =(550,0,250,200)
+        char_stats = pygame.draw.rect(base_surf,Green,x_y_wid_hei)
+        # todo: put this in a thread that watches the Hero class for xp_events
+
+        self.render_xp_bar(base_surf, hero)
+        self.render_hp_bar(base_surf, hero)
+
+#         TODO: Create this Subsurface for live updating from data
+
+    '''Follows are all the methods for rendering the components of the Stat Page'''
+    @staticmethod
+    def render_other_char_data(hero):
+        hero_name = hero.name
+        hero_str = hero.strength
+        hero_armor = hero.armor
+        hero_level = hero.level
+        hero_money = hero.money
+    # Follows are a bunch of text box renders
+    # fontObj = pygame.font.Font('freesansbold.ttf',12)
+    # textSurfaceObj = fontObj.render('Hello World Text!', True, White, Black)
+    # textRectObj = textSurfaceObj.get_rect()
+        base_font = pygame.font.Font('freesansbold.ttf', 12)
+        big_font = pygame.font.Font('freesansbold.ttf',14)
+
     def render_xp_bar(self,base_surf, hero):
-        x_y_wid_hei_for_outline = (575, 180, 200, 15)
+        x_y_wid_hei_for_outline = (575, 180, 200, 10)
         fill_width = self.determine_xp_bar(hero.xp, hero.next_lvl)
-        x_y_wid_hei_for_fill = (576, 181, fill_width, 13)
+        x_y_wid_hei_for_fill = (576, 181, fill_width, 8)
         empty_bar = pygame.draw.rect(base_surf, Black, x_y_wid_hei_for_outline, 1)
-        fill_bar = pygame.draw.rect(base_surf, White, x_y_wid_hei_for_fill)
+        fill_bar = pygame.draw.rect(base_surf, Green, x_y_wid_hei_for_fill)
 
     @staticmethod
     def determine_xp_bar(xp, next_lvl):
@@ -35,12 +67,21 @@ class Main_Display:
         bar_width = math.floor(percent * 198)
         return bar_width
 
-    def render_char_stat_display(self,base_surf,hero):
-        x_y_wid_hei =(550,0,250,200)
-        char_stats = pygame.draw.rect(base_surf,Green,x_y_wid_hei)
-        # todo: put this in a thread that watches the Hero class for xp_events
-        self.render_xp_bar(base_surf,hero)
-#         TODO: Create this Subsurface for live updating from data
+    def render_hp_bar(self,base_surf,hero):
+        x_y_wid_hei_for_outline = (575, 40, 200, 12)
+        fill_width = self.determine_hp_bar(hero.current_hp, hero.max_hp)
+        x_y_wid_hei_for_fill = (576, 41, fill_width, 10)
+        empty_bar = pygame.draw.rect(base_surf,Black, x_y_wid_hei_for_outline,1)
+        fill_bar = pygame.draw.rect(base_surf,Red, x_y_wid_hei_for_fill)
+
+    @staticmethod
+    def determine_hp_bar(cur_hp,max_hp):
+    #     198 pixels
+        piece = math.floor(198/max_hp)
+        length = piece*cur_hp
+        if cur_hp == max_hp:
+            length = 198
+        return length
 
     def render_basic_commands(self,base_surf):
         x_y_wid_hei =(550,200,250,200)
@@ -68,36 +109,36 @@ class Main_Display:
 
 
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Hello World')
-
-fontObj = pygame.font.Font('freesansbold.ttf',12)
-textSurfaceObj = fontObj.render('Hello World Text!', True, White, Black)
-textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (200,150)
-# x,y of top left corner and then w&h
-char_stat_box_dimensions = (550,0,250,200)
-command_box_dimensions = (550,200,250,200)
-inventory_box_dimensions = (550,400,250,200)
-
-battle_log = (0,500,550,100)
-
-while True:
-    DISPLAYSURF.fill(White)
-    pygame.draw.rect(DISPLAYSURF, Red, char_stat_box_dimensions)
-    pygame.draw.rect(DISPLAYSURF, Green, command_box_dimensions)
-    pygame.draw.rect(DISPLAYSURF, Purple, inventory_box_dimensions)
-    pygame.draw.rect(DISPLAYSURF, Blue, battle_log)
-    # DISPLAYSURF.blit(textSurfaceObj, textRectObj)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
-
-
-
-def draw_char_status():
-
-    '''THIS SECTION OF CODE MOVES A CAT IMAGE AROUND, BASIC Animation'''
+# pygame.init()
+# DISPLAYSURF = pygame.display.set_mode((800,600))
+# pygame.display.set_caption('Hello World')
+#
+# fontObj = pygame.font.Font('freesansbold.ttf',12)
+# textSurfaceObj = fontObj.render('Hello World Text!', True, White, Black)
+# textRectObj = textSurfaceObj.get_rect()
+# textRectObj.center = (200,150)
+# # x,y of top left corner and then w&h
+# char_stat_box_dimensions = (550,0,250,200)
+# command_box_dimensions = (550,200,250,200)
+# inventory_box_dimensions = (550,400,250,200)
+#
+# battle_log = (0,500,550,100)
+#
+# while True:
+#     DISPLAYSURF.fill(White)
+#     pygame.draw.rect(DISPLAYSURF, Red, char_stat_box_dimensions)
+#     pygame.draw.rect(DISPLAYSURF, Green, command_box_dimensions)
+#     pygame.draw.rect(DISPLAYSURF, Purple, inventory_box_dimensions)
+#     pygame.draw.rect(DISPLAYSURF, Blue, battle_log)
+#     # DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+#     pygame.display.update()
+#
+#
+#
+# def draw_char_status():
+#
+#     '''THIS SECTION OF CODE MOVES A CAT IMAGE AROUND, BASIC Animation'''
---
+++ text_blit.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class text_blit:
+    def __init__(self):
+        battle_box_x = 0
+        battle_box_y = 0
