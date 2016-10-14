f04668e748f0f6c5652423f60864d92ff464fd6c
ShivonQ <realitykeeper@gmail.com>
2016-10-14 15:49:42 -0500

    Procedural map generator works[Small Problem with Occasional Index problems]

--- map.py
+++ map.py
@@ -7,9 +7,7 @@ from pygame.locals import *
 import pcdungeon as pcd
 
 
-
 class Map_Maker:
-
     def __init__(self):
 
         # http://usingpython.com/adding-an-inventory/
@@ -34,17 +32,17 @@ class Map_Maker:
         }
         # These variables comtrol the generation of rooms, how many, max and min size.
         # TODO: Make these reactive to the level of the hero?
-        self.MAX_ROOMS = 15
+        self.MAX_ROOMS = 20
         self.min_ROOMSIZE = 3
         self.max_ROOMSIZE = 8
 
         self.TILE_SIZE = 25
-        self.MAP_WIDTH = 50
-        self.MAP_HEIGHT = 50
+        self.MAP_WIDTH = 51
+        self.MAP_HEIGHT = 51
         self.SCREEN_WIDTH = 22
         self.SCREEN_HEIGHT = 20
 
-        self.resources = [WALL,WATER,DOOR,WOOD,HALL,PENT]
+        self.resources = [WALL, WATER, DOOR, WOOD, HALL, PENT]
         # TODO: make sure starter map is renamed throughout document
         self.starter_map = [[WALL for w in range(self.MAP_WIDTH)] for h in range(self.MAP_HEIGHT)]
 
@@ -69,8 +67,8 @@ class Map_Maker:
                     # TODO: for room in all_rooms: replace the wall sections with one of two types of floor sections(WOOD, HALL)
                     center = this_room.center
                     if len(all_rooms) != 0:
-                        prev_center = all_rooms[len(all_rooms)-1].center
-                        if random.randint(1,2)==1:
+                        prev_center = all_rooms[len(all_rooms) - 1].center
+                        if random.randint(1, 2) == 1:
                             h_corridors(prev_center[0], center[0], prev_center[1])
                             v_corridor(prev_center[1], center[1], prev_center[0])
                         else:
@@ -86,26 +84,26 @@ class Map_Maker:
         def carve_all_rooms(all_rooms):
 
             for room in all_rooms:
-                wood_or_hall = random.randint(1,2)
+                wood_or_hall = random.randint(1, 2)
                 if wood_or_hall == 1:
                     tile_type = HALL
                 else:
                     tile_type = WOOD
                 for row in range(room.height):
                     for col in range(room.width):
-                        self.starter_map[room.x1+row][room.y1+col] = tile_type
+                        self.starter_map[room.x1 + row][room.y1 + col - 1] = tile_type
 
         '''MAKE SOME CORRIDORS'''
-        def h_corridors(x1,x2,y):
-            for x in range(min(x1,x2)+1,max(x1,x2)+1):
+
+        def h_corridors(x1, x2, y):
+            for x in range(min(x1, x2) + 1, max(x1, x2) + 1):
                 # Place a Hall Block in this cell.
                 self.starter_map[x][y] = HALL
 
-        def v_corridor(y1,y2,x):
-            for y in range(min(y1,y2)+1,max(y1,y2)+1):
+        def v_corridor(y1, y2, x):
+            for y in range(min(y1, 2) + 1, max(y1, y2) + 1):
                 # Place a hall in this location
                 self.starter_map[x][y] = HALL
-
 
         # def modify_map_tiles(map_to_mod):
         #     for rw in range(self.MAP_HEIGHT):
@@ -129,4 +127,4 @@ class Map_Maker:
 
 mm = Map_Maker
 mm()
-            # SURF.blit(textures[rand_tile_map[row][column]],(column*TILE_SIZE,row*TILE_SIZE))
+# SURF.blit(textures[rand_tile_map[row][column]],(column*TILE_SIZE,row*TILE_SIZE))
